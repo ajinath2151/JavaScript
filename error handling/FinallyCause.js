2d7ae6effe3df2e956664821661b93b1484da33c
Ajinath <ajinath2151@gmail.com>
2023-10-29 21:03:12 +0530

    add finally block example of try catch

---
+++ error handling/FinallyCause.js
@@ -0,0 +1,20 @@
+// to run this file after navigate in terminal write node FinallyCause.js
+const f = () => {
+  try {
+    let a = 0;
+    console.log(program);
+    console.log("inside try block");
+  } catch (error) {
+    console.log("inside catch block");
+    console.log(p);
+  } finally {
+    console.log("this is finally block always run");
+    // user for like
+    // close the File
+    // exit to loop
+    // write default value
+    // also run if error not occure 
+    // also run if return something form try
+  }
+};
+f();
