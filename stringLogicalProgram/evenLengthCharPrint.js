8b06bbd11cc67a9a4fb1bc8b2a5136d0cc8ae247
Ajinath <ajinath2151@gmail.com>
2023-12-08 22:50:41 +0530

    print even length character form string

---
+++ stringLogicalProgram/evenLengthCharPrint.js
@@ -0,0 +1,10 @@
+
+// print even characters form string
+
+const evenCharPrint = (str) => {
+    for(let i=0;i<str.length;i+=2) {
+        console.log(str[i]);
+    }
+}
+let inputStr = "ajinath";
+evenCharPrint(inputStr);
