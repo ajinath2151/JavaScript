c32c931aef140981757d4d4a990a34f4b1abc9d8
Ajinath <ajinath2151@gmail.com>
2023-12-08 23:04:29 +0530

    merge two string together in two ways by return keyword and direct print

---
+++ stringLogicalProgram/MergeTwoString.js
@@ -0,0 +1,16 @@
+
+// merge two string into one
+
+// const mergeFuncitn = (str1, str2) => {
+//     let strResult = str1 + str2;
+//     console.log(strResult);
+// }
+// mergeFuncitn("ajinath","nobody");
+
+const mergeFuncitn = (str1, str2) => {
+    let strResult = str1 + str2;
+    return strResult;
+}
+let result = mergeFuncitn("ajinath","nobody");
+console.log(result);
+
