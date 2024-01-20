885c1d0d7f3c6746e98eda93b2842b6eefaef103
Ajinath <ajinath2151@gmail.com>
2024-01-20 17:28:44 +0530

    char count program of sting

---
+++ stringLogicalProgram/CharCount02.js
@@ -0,0 +1,7 @@
+
+//character count form string
+
+let str = "ajinath shinde";
+console.log("string length: " + str +  " " + str.length);
+let arr = str.split(" ");
+console.log("string after convert into array : " + arr);
