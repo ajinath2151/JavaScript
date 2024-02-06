ba49101a8dc5975c514263594f2e2fe1ed3329b0
Ajinath <ajinath2151@gmail.com>
2024-02-06 18:36:07 +0530

    add element in blank array using function and without function

---
+++ arrayLogicalPrograms/LogicalPrograms/AddElementInBlankArray.js
@@ -0,0 +1,25 @@
+
+let arr = [];
+arr.push(10);
+arr.push(20);
+arr.push(30);
+arr.push(40);
+console.log(arr);
+arr.push(50);
+arr.push(60);
+console.log(arr);
+
+// with function
+function myArray(element) {
+    // arr2.push(10);
+    // arr2.push(20);
+    // arr2.push(30);
+    // arr2.push(40);
+    console.log(arr2);
+    // arr2.push(50);
+    // arr2.push(60);
+    // console.log(arr2);    
+}
+let arr2 = [];
+arr2 = myArray(arr2=[10,20,30,40]);
+arr2 = myArray(arr2=[10,20,30,40,50,60]);
