b5c7f6aaf73669f9eee29268297cf08a75dbfbf2
Ajinath <ajinath2151@gmail.com>
2024-02-23 00:20:33 +0530

    null and undefined difference

---
+++ practice/NullAndUndefined.js
@@ -0,0 +1,8 @@
+
+// null - null in js is a empty value and also primitive data type. this variable which assigned null contains no value.
+// undefined - variable has been declared and value not assigned.
+
+let a= null;
+console.log(a);
+let b= undefined;
+console.log(b);
