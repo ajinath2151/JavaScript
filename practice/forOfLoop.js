e3a931926b8770df177ce02f2f858b410f9ca654
Ajinath <ajinath2151@gmail.com>
2024-02-26 22:04:55 +0530

    for of (forOf) loop example wiht array and string

---
+++ practice/forOfLoop.js
@@ -0,0 +1,12 @@
+// for of loop of javascript demo with array
+let arr = [1, 2, 3, 4, 5];
+for(let i of arr) {
+    console.log(i);
+}
+
+// for of loop of javascript demo with string
+
+let str = 'hello world';
+for(let i of str) {
+    console.log(i);
+}
