39250b85b8fea0d055dffef96f0f3f42ab10aeca
Ajinath <ajinath2151@gmail.com>
2024-09-20 23:15:48 +0530

    array method lenght and toString

---
+++ arrayLogicalPrograms/program3/ArrayMethods.js
@@ -0,0 +1,27 @@
+
+// 1: length method - it return count/size of array.array
+
+let arrLen =  [1,2,3,4,5,6,7,8];
+console.log("array length is : "  + arrLen.length);
+
+let arrFruits = ["banana","apple","orange","pineapple"];
+let len = arrFruits.length;
+console.log("fruits array length " + len);
+
+// 2: toString - converts an array into a string, with each element of the array separated by commas.
+
+let fruits = ["banana","apple","orange","pineapple"];
+console.log("before array to string: " + fruits);
+let fruitsString = fruits.toString();
+console.log("after array to string: " + fruitsString);
+// Create an array
+let fruits2 = ['Apple', 'Banana', 'Cherry', 'Mango'];
+
+// Convert the array to a string
+let fruitsString2 = fruits2.toString();
+
+console.log(fruitsString2); // Output: "Apple,Banana,Cherry,Mango"
+
+
+
+
